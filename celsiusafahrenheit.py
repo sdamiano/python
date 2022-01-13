24f7ce14a2f2cc7657c19bdf0de3679fc7fbc962
Sebastián Damiano <sebamardamiano@gmail.com>
2022-01-12 23:07:58 -0300

    Add files via upload
    Conversión de grados CELSIUS A FAHRENHEIT mediante el llamado de una función.

---
+++ celsiusafahrenheit.py
@@ -0,0 +1,6 @@
+def convertir():
+    return (C * 1.8 ) + 32
+
+C = float(input("Ingrese temperatura en grados en escala Celsius o centígrada: "))
+
+print(C,"grados CELSIUS equivalen a",convertir(),"grados FAHRENHEIT")
